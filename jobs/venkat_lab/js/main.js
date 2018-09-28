bfb1860657697fe0ecfb22e273fb78952103f91d
Riddhi A. Mandal <archonriddhi@gmail.com>
2018-09-29 00:26:42 +0530

    -

--- jobs/venkat_lab/js/main.js
+++ jobs/venkat_lab/js/main.js
@@ -84,23 +84,130 @@ $('.p-mid').html(markdown.toHTML(publication_list))
 $('.current-email').html(markdown.toHTML(email))
 $('.current-address').html(markdown.toHTML(physicaladdr))
 
-$('#query-form').submit(function(event){
+$('#query-form').submit(function(event) {
     let formData = {
-        'name' : $('input[name=name]').val(),
-        'email' : $('input[name=email]').val(),
-        'comments' : $('textarea[name=comments]').val()
+        'name': $('input[name=name]').val(),
+        'email': $('input[name=email]').val(),
+        'comments': $('textarea[name=comments]').val()
     }
     $.ajax({
-            type : 'POST',
-            url : 'mail.php',
-            data : formData,
-            dataType : 'json',
-            encode : true
-        }).done(function(data) {
-            $('#query-form').trigger("reset");
-        });
-        alert('Your message has been sent');
-        
+        type: 'POST',
+        url: 'mail.php',
+        data: formData,
+        dataType: 'json',
+        encode: true
+    }).done(function(data) {
+        $('#query-form').trigger("reset");
+    });
+    alert('Your message has been sent');
+
     event.preventDefault()
 
 })
+
+
+$('.supervisor').html('<div class="head-card"><div class="info-wrapper"><h2 class="title"></h2><h2 class="name"></h2><div class="info-wrapper-l"><p class="emails"></p><p class="positions"></p></div><div class="info-wrapper-r"><p class="qualifications"></p></div></div><div class="image-wrapper"></div></div>')
+$('.supervisor .head-card .title').html(head_card.title)
+$('.supervisor .head-card .name').html(head_card.name)
+
+html_str = '<strong>Emails:</strong><br>'
+for (i = 0; i < head_card.email.length; i++) {
+    html_str = `${html_str}${head_card.email[i]}<br>`
+}
+
+$('.supervisor .emails').html(html_str)
+
+html_str = '<h3>Positions:</h3><br>'
+for (i = 0; i < head_card.positions.length; i++) {
+    html_str = `${html_str}${head_card.positions[i]}<br>`
+}
+
+$('.supervisor .positions').html(html_str)
+
+html_str = '<h3>Qualifications:</h3><br>'
+for (i = 0; i < head_card.qualifications.length; i++) {
+    html_str = `${html_str}${head_card.qualifications[i]}<br>`
+}
+
+$('.supervisor .qualifications').html(html_str)
+
+$('.supervisor .image-wrapper').html(`<img src='img/${head_card.photo}' />`)
+
+
+html_str = ''
+for (i = 0; i < postdocs.length; i++) {
+    html_str = `${html_str}<div class="card"><div class="card-img"><img src="img/${postdocs[i].photo}" /></div><div class="card-info">Name: <strong>${postdocs[i].name}</strong><br>Position: <strong>${postdocs[i].position}</strong><br>Duration: <strong>${postdocs[i].duration}</strong><br>Topic: <strong>${postdocs[i].topic}</strong><br>Profile: <strong>${postdocs[i].profile}</strong><br>Email: <strong>${postdocs[i].email}</strong></div></div>`
+}
+
+$('.postdoc').html(html_str)
+
+html_str = ''
+for (i = 0; i < phds.length; i++) {
+    html_str = `${html_str}<div class="card"><div class="card-img"><img src="img/${phds[i].photo}" /></div><div class="card-info">Name: <strong>${phds[i].name}</strong><br>Position: <strong>${phds[i].position}</strong><br>Duration: <strong>${phds[i].duration}</strong><br>Topic: <strong>${phds[i].topic}</strong><br>Profile: <strong>${phds[i].profile}</strong><br>Email: <strong>${phds[i].email}</strong><br>Contact: <strong>${phds[i].contact}</strong></div></div>`
+}
+
+$('.phd').html(html_str)
+
+html_str = ''
+for (i = 0; i < iphds.length; i++) {
+    html_str = `${html_str}<div class="card"><div class="card-img"><img src="img/${iphds[i].photo}" /></div><div class="card-info">Name: <strong>${iphds[i].name}</strong><br>Position: <strong>${iphds[i].position}</strong><br>Duration: <strong>${iphds[i].duration}</strong><br>Topic: <strong>${iphds[i].topic}</strong><br>Profile: <strong>${iphds[i].profile}</strong><br>Email: <strong>${iphds[i].email}</strong><br>Contact: <strong>${iphds[i].contact}</strong></div></div>`
+}
+
+$('.iphd').html(html_str)
+
+html_str = ''
+for (i = 0; i < projectstudents.length; i++) {
+    html_str = `${html_str}<div class="card"><div class="card-img"><img src="img/${projectstudents[i].photo}" /></div><div class="card-info">Name: <strong>${projectstudents[i].name}</strong><br>Position: <strong>${projectstudents[i].position}</strong><br>Topic: <strong>${projectstudents[i].topic}</strong><br>Email: <strong>${projectstudents[i].email}</strong><br>Contact: <strong>${projectstudents[i].contact}</strong></div></div>`
+}
+
+$('.project').html(html_str)
+
+html_str = ''
+for (i = 0; i < visiting.length; i++) {
+    html_str = `${html_str}<div class="card"><div class="card-img"><img src="img/${visiting[i].photo}" /></div><div class="card-info">Name: <strong>${visiting[i].name}</strong><br>Position: <strong>${visiting[i].current_position}</strong><br>Duration: <strong>${visiting[i].duration}</strong><br>Topic: <strong>${visiting[i].topic}</strong><br>Email: <strong>${visiting[i].email}</strong><br>Contact: <strong>${visiting[i].contact}</strong></div></div>`
+}
+
+$('.visitors').html(html_str)
+
+html_str = ''
+for (i = 0; i < alumni.length; i++) {
+    html_str = `${html_str}<div class="card"><div class="card-img"><img src="img/${alumni[i].photo}" /></div><div class="card-info">Name: <strong>${alumni[i].name}</strong><br>`
+    if (alumni[i].position != null) {
+        if (alumni[i].position.length > 0) {
+            html_str = `${html_str}Position: <strong>${alumni[i].position}</strong><br>`;
+        }
+    }
+    if (alumni[i].current_position != null){
+        if (alumni[i].current_position.length > 0) {
+            html_str = `${html_str}Current Position: <strong>${alumni[i].current_position}</strong><br>`;
+        }
+    }
+    if (alumni[i].duration != null) {
+        if (alumni[i].duration.length > 0) {
+            html_str = `${html_str}Duration: <strong>${alumni[i].duration}</strong><br>`;
+        }
+    }
+    if (alumni[i].topic != null) {
+        if (alumni[i].topic.length > 0) {
+            html_str = `${html_str}Research Topic: <strong>${alumni[i].topic}</strong><br>`;
+        }
+    }
+    if (alumni[i].profile != null) {
+        if (alumni[i].profile.length > 0) {
+            html_str = `${html_str}Profile: <strong>${alumni[i].profile}</strong><br>`;
+        }
+    }
+    if (alumni[i].email != null) {
+        if (alumni[i].email.length > 0) {
+            html_str = `${html_str}Email: <strong>${alumni[i].email}</strong><br>`;
+        }
+    }
+    if (alumni[i].contact != null) {
+        if (alumni[i].contact.length > 0) {
+            html_str = `${html_str}Contact: <strong>${alumni[i].contact}</strong><br>`;
+        }
+    }
+    html_str = `${html_str}</div></div>`
+}
+
+$('.alumni').html(html_str)
---
+++ jobs/venkat_lab/txt/group.js
@@ -0,0 +1,263 @@
+let head_card={
+	title: "Group Supervisor",
+	name: "Dr. Venkatramanan Mahalingam",
+	photo: "1453113520.png",
+	positions: ["Associate Professor (2014-present)","Assistant Professor (2009-2013)","Department of Chemical Sciences,Indian Institute of Science Education and Research Kolkata (IISER K) Mohanpur Campus, Nadia, 741252, West Bengal, India","Editorial Board Member, Scientific Reports"],
+	email: ["mvenkataramanan@yahoo.com","mvenkat@iiserkol.ac.in"],
+	qualifications: ["B. Sc.,Chemistry, American College, Madurai, (1994)","M. Sc., Chemistry, American College, Madurai, (1996)","PhD, Chemistry, under Prof. T. Pradeep, Indian Institute of Technology, Madras (IIT-M), (2001)","Post doctoral scholar, under Prof. D. N. Reinhoudt, University of Twente, Netherlands (Oct 2001-Oct 2003)","Post doctoral Fellow, under Prof. FCJM van Veggel, University of Victoria, Canada (April 2004-April 2007)","Post doctoral Associate, under Prof. John. A. Capobianco, Concordia University, Canada (May 2007- April, 2009)","Visiting Faculty, University of Denver, Colorado, USA  (July -August 2012)"]
+}
+
+let postdocs=[
+{
+	name: "Dr. Sourav Ghosh",
+	position: "Post-doc Fellow (NPDF), IISER Kolkata",
+	duration: "(Aug, 2017)-present",
+	topic: "Fabrication of porous transition metal oxide nano-structure for next generation sodium ion batteries and super-capacitors",
+	profile: "Ph.D., CGCRI",
+	email: "sourav.g1989@gmail.com",
+	photo: "sourav-ghosh.jpg"
+},
+{
+	name: "Dr. Mohua Chakraborty",
+	position: "Post-doc Fellow (NPDF), IISER Kolkata",
+	duration: "(Oct, 2017)-present",
+	topic: "Metal oxide thin films for solar cell and photoelectrochemical water splitting applications",
+	profile: "Ph.D., IIT (ISM) Dhanbad",
+	email: "mchakraborty1988@gmail.com",
+	photo: "mahuya.jpg"
+}
+]
+
+
+let phds=[
+{
+	name: "A.V. N. Kishor Babu",
+	position: "SRF (UGC), IISER Kolkata",
+	duration: "(Jan, 2013)-present",
+	topic: "Lanthanide based luminescent nanomaterials for LED, sensing and bio-applications",
+	profile: "M.Sc., Kakatiya University",
+	email: "kishor.adusumalli8@gmail.com",
+	contact: "07602894385",
+	photo: "3258617.jpg"
+},
+{
+	name: "Tanmoy Biswas",
+	position: "SRF (UGC), IISER Kolkata",
+	duration: "(Feb, 2014)-present",
+	topic: "CO2 fixation into useful chemicals and fuel",
+	profile: "M.Sc., IIT-Kharagpur",
+	email: "biswastanmoy88@gmail.com",
+	contact: "08967730971",
+	photo: "4199023.jpg"
+},
+{
+	name: "Harish Reddy Inta",
+	position: "SRF (UGC), IISER Kolkata",
+	duration: "(Jul, 2014)-present",
+	topic: "Graphene based nanocomposites and applications",
+	profile: "M.Sc., University of Hyderabad",
+	email: "harishreddy9396@gmail.com",
+	contact: "07076630073",
+	photo: "6664494.jpg"
+},
+{
+	name: "Sagar Ganguli",
+	position: "SRF (CSIR), IISER Kolkata",
+	duration: "(Jul, 2014)-present",
+	topic: "Synthesis of transition metal based electrocatalysts for energy storage",
+	profile: "MS, IISER Kolkata",
+	email: "gangulisagar@gmail.com",
+	contact: "gangulisagar.weebly.com,09836650751, 08250251152",
+	photo: "6495191.jpg"
+},
+{
+	name: "Gouri Tudu",
+	position: "SRF (UGC), IISER-Kolkata",
+	duration: "(Jul, 2015)-present",
+	topic: "Synthesis and Property Studies of Ln3+-based Nanoparticle-Hydrogel Composites for Sensing and Biomedical Applications",
+	profile: "M.Sc., IIT-Kharagpur",
+	email: "chem.gouri@gmail.com",
+	contact: " 09832512591",
+	photo: "6765143.jpg"
+},
+{
+	name: "Athma E Praveen",
+	position: "JRF (INSPIRE), IISER-Kolkata",
+	duration: "(Jul, 2017)-present",
+	topic: "Synthesis of hetero-junction nano-structures for photo and electrocatalysis",
+	profile: "BS-MS, IISER Kolkata",
+	email: "athmaep@gmail.com",
+	contact: "08017125524",
+	photo: "athma.png"
+}
+]
+
+let iphds=[
+{
+	name: "Debashrita Sarkar",
+	position: "SRF (IISER Fellow), IISER-Kolkata",
+	duration: "(Jul, 2015)-present",
+	topic: "Caltalytic activation of silicate materials",
+	profile: "Integrated MS-Ph.D. student, IISER Kolkata",
+	email: "debashrita1chemistry@gmail.com",
+	contact: "09563165894",
+	photo: "1453204988.png"
+},
+{
+	name: "Khushboo Paliwal",
+	position: "2nd year IPhD Project (IISER Fellow), IISER-Kolkata",
+	duration: "(May, 2018)-present",
+	topic: "CO2 fixation by polymeric materials",
+	profile: "Integrated MS-Ph.D. student, IISER Kolkata",
+	email: "khushboopaliwal@gmail.com",
+	contact: "08788713249",
+	photo: "photo.jpg"
+}
+]
+
+let projectstudents=[
+{
+	name: "Rahul Kumar",
+	position: "4th year Project, IISER-Kolkata",
+	topic: "Ternary metal chalcogenides for electrocatalysis",
+	email: "rkrahulkumar063@gmail.com",
+	contact: "07063265389",
+	photo: "14241476-1087649264651080-2776107237454309044-o.jpeg"
+},
+{
+	name: "Soumik Das",
+	position: "4th year Project, IISER-Kolkata",
+	topic: "Hydrogen evolution reaction using transition metal compounds",
+	email: "soumik.mm@gmail.com",
+	contact: "09831499435",
+	photo: "9459788.jpg"
+}
+]
+
+let visiting=[
+{
+	name: "Dr. Amlan Kumar Pal",
+	current_position: "Leverhulme Early Career Fellow",
+	duration: "(May 2018-July 2018)",
+	topic: "Supramolecular Chemistry and Photophysics",
+	email: "akp5@st-andrews.ac.uk",
+	contact: "+44-7759495542",
+	photo: "importedprofilephoto.jpg"
+}
+]
+
+let alumni=[
+{
+	name: "Nikita Madhukar",
+	position: "5th year Project, IISER-Kolkata",
+	topic: "Sensing of anions using Ln3+ doped nanocrystals",
+	photo: "nikkita.jpg"
+},
+{
+	name: "Aswanth V P",
+	position: "5th year Project, IISER-Kolkata",
+	topic: "Synthesis of hetero-junction nano-composites for catalysis",
+	email: "aswanthpurameri@gmail.com",
+	contact: "08170896672",
+	photo: "aswanth-2.jpeg"
+},
+{
+	name: "K. H. V. S. R. Murthy",
+	position: "5th year MS project, IISER-Kolkata",
+	topic: "Lanthanide based luminescent nanomaterials",
+	email: "sairam2ks@gmail.com",
+	contact: "09547836495",
+	photo: "1474547074.png"
+},
+{
+	name: "Dr. Tuhin Samanta",
+	current_position: "Postdoctoral Fellow, KIT, Germany",
+	duration: "(Jan, 2013- March, 2017)",
+	topic: "Sensitization of lanthanide ions luminescence and it's applications",
+	email: " tuhinchem.164@gmail.com",
+	contact: "8927297120",
+	photo: "tuhin3.jpeg"
+},
+{
+	name: " Dr. Abhisek Brata Ghosh",
+	current_position: "Postdoctoral Fellow ",
+	duration: "(Aug, 2017- Nov, 2017)",
+	topic: "Graphene based nanocomposites for photocatalysis",
+	profile: "Ph.D., IIEST",
+	email: "abhibrata.ghosh@gmail.com",
+	photo: "sourav-ghosh.png"
+},
+{
+	name: "Dr. Brahmaiah Meesarangandla",
+	current_position: "Postdoctoral Fellow, University of Greifswald, Germany",
+	duration: "(Dec, 2011- 2017)",
+	topic: "Development of  Synthetic Routes to make Water Dispersible Upconverting Nanocrystals and their applications",
+	email: "brahmi.msc@gmail.com",
+	photo: "brahmi.jpg"
+},
+{
+	name: "Akash Verma",
+	position: "",
+	current_position: "",
+	duration: "",
+	topic: "Synthesis of Ln3+ doped NaYF4 Hydrogel Nanocomposites",
+	profile: "",
+	email: "verma.akash2111@gmail.com",
+	contact: "8017482474",
+	photo: "4286068.jpg"
+},
+{
+	name: "Dr. Sourav Kanti Jana",
+	current_position: "Postdoctoral Fellow, IIT Madras",
+	duration: "(Jan, 2015- Dec, 2016)",
+	topic: "Design and fabrication of electrochemical energy genaration, storage and sensing device using 2D nanostructures",
+	profile: "Ph.D., University of Milano-Bicocca<br>Research Associate, SINP, Kolkata",
+	email: "skj_ec@yahoo.co.in",
+	photo: "sourav.jpg"
+},
+{
+	name: " Manjunath Chatti",
+	current_position: "Ph.D. Fellow, Monash University, Australia",
+	email: "manjunath.chatti@monash.edu",
+	photo: "manjunath.jpg"
+},
+{
+	name: "Rahul Kumar Baidya",
+	current_position: "Working with Teach For Green",
+	topic: "Metal nanoparticles' plasmon induced enhancement of upconversion luminescence",
+	profile: "",
+	email: "kumar.rahul534@gmail.com",
+	contact: "+91-8340714075, +91-8603452763",
+	photo: "rahul-kumar.jpg"
+},
+{
+	name: "Dr. Armita Dash",
+	current_position: "Ph.D. Fellow, University of Victoria, Canada",
+	photo: "armita-1.jpeg"
+},
+{
+	name: "Swapil Paliwal",
+	current_position: "Apex Learning",
+	profile: "Masters Student, University of Washington, USA.<br>Masters Student, University of Pennsylvania, USA.",
+	email: "swapilpaliwal@gmail.com",
+	photo: "swapil.jpeg"
+},
+{
+	name: "Aditi Chandrasekar",
+	current_position: "Ph.D. Fellow, Indira Gandhi Centre for Atomic Research, Kalpakkam",
+	email: "aditic2003@gmail.com",
+	photo: "aditi.jpg"
+},
+{
+	name: "Rahul Kumar Mishra",
+	current_position: "Ph.D. Fellow, Bar-Ilan University, Israel",
+	email: "rahuliiserk@gmail.com",
+	photo: "rahul.jpg"
+},
+{
+	name: "Debadrita Bhattacharyya",
+	current_position: "Ph.D. Fellow, IIT-Kanpur, India",
+	email: "bdebadrita@gmail.com",
+	photo: "debi.jpg"
+}
+]
